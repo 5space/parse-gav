5c967c0cd6a9cbad87134083b3af31139b4a5efe
5space <holdenew@gmail.com>
2022-08-24 09:37:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const fs = require("fs");
 const fileName = core.getInput("file") || "pom.xml";
 fs.readFile(fileName, function(err, result) {
     if (err) {
-        core.error(err.message);
+        core.setFailed(err.message);
         return;
     }
 
     parse(result, function(err, xml) {
         if (err) {
-            core.error(err.message);
+            core.setFailed(err.message);
             return;
         }
         if (!xml.project || !(xml.project.groupId || xml.project.parent[0].groupId) || !xml.project.artifactId || !xml.project.version) {
-            core.error("Invalid pom.xml structure");
+            core.setFailed("Invalid pom.xml structure");
             return;
         }
         core.setOutput("groupId", xml.project.groupId ? xml.project.groupId[0] : xml.project.parent[0].groupId[0]);
